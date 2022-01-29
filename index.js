dceff0a50c443f0d42773df3a3a1f5e2447c765b
Zsvob <svobodazachary@gmail.com>
2022-01-29 10:21:44 -0500

    questions

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const Employee = require('./lib/Employee.js');
-const Engineer = require('./lib/Engineer.js');
-const Manager = require('./lib/Manager.js');
 const inquirer = require('inquirer');
-// const generateHTML = require('');
 
 
 const promptEmployee = () => {
@@ -27,70 +23,130 @@ const promptEmployee = () => {
             name: 'role',
             message: 'Enter Employee Role:',
             choices: ['Manager', 'Engineer', 'Intern']
-        },    
+        },
     ])
+}
+
+const promptRole = employeeData => {
+    if (employeeData.role === 'Manager') {
+        if (!employeeData.officeNumber) {
+            employeeData.officeNumber = '';
+        }
+
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter an office number',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter an office number!');
+                        return false;
+                    }
+                }
+
+            },
+
+        ]).then(projectData => {
+            employeeData.officeNumber = projectData;
+            return employeeData;
+        });
+    }
+    if (employeeData.role === 'Engineer') {
+        if (!employeeData.github) {
+            employeeData.github = '';
+        }
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: "Enter the Engineer's GitHub:",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please Enter the employees gitHub');
+                        return false;
+                    }
+                }
+
+            },
+
+        ]).then(projectData => {
+            employeeData.github = projectData;
+            return employeeData;
+
+        });
+    }
+    if (employeeData.role === 'Intern') {
+        if (!employeeData.college) {
+            employeeData.college = '';
+        }
+
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'college',
+                message: "Please Enter the Intern's College",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a College');
+                        return false;
+                    }
+                }
+            },
+
+        ]).then(projectData => {
+            employeeData.college = projectData;
+
+            return employeeData;
+        });
+    }
+
+
+};
+
+const promptAddEmployee = employeeData => {
     
+        if (!employeeData.confirmAdd) {
+            employeeData.confirmAdd = '';
+        }
+
+        return inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'confirmAdd',
+                message: 'Would you like to add another Employee?',
+                default: false
+
+            },
+
+        ]).then(projectData => {
+            employeeData.confirmAdd = projectData;
+            return employeeData;
+        });
+    }
+let dataArray = []
+
+const compiler = data => {
+    dataArray.push(data);
+    console.log(dataArray);
+    if(data.confirmAdd.confirmAdd) {
+        employeeLoop();   
+    } else {
+        generateHTML(dataArray)
+    }
+
+};
+
+const employeeLoop = () => {
+    promptEmployee()
+        .then(promptRole)
+        .then(promptAddEmployee)
+        .then(compiler)
 }
-
-const promptManager = (data) => {
-    data.officenumber = 
-     inquirer.prompt([
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "Enter the Manager's Office Number:",
-        },
-    ])
-    console.log(data)
-}
-
-const promptEngineer = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: "Enter the Engineer's GitHub:",
-        },
-    ])
-}
-
-const promptIntern = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'college',
-            message: "Enter the Interns College:",
-        },
-
-    ])
-}
-
-     
-
-
-promptEmployee()
-// .then(employeeData => {
-//     if(employeeData.role === 'Manager') {
-//         promptManager(employeeData)
-//     }   
-//     if(employeeData.role === 'Engineer') {
-//         promptEngineer()
-//     }
-//     if(employeeData.role === 'Intern') {
-//         promptIntern()
-//     }  
-// })
-
-    
-
-
-
-
-// .then(data => {
-//     console.log(data)
-    //return generateHTML(data)
-
-
-
-
-
+employeeLoop();
